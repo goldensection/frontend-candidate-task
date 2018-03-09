d769b6f1ddd0d681eddec201acde9d03a79e5cd3
TN1ck <tomwanick@gmail.com>
2018-03-09 11:52:29 +0100

    feat: basic implementation, create questions

--- README.md
+++ README.md
@@ -8,26 +8,32 @@ Each cell interacts with its eight neighbors (horizontal, vertical, diagonal).
 
 Game of life is defined with these rules:
 
-1. Any live cell with fewer than two live neighbors dies, as if cased by under-population
+1. Any live cell with fewer than two live neighbors dies, as if caused by under-population.
 2. Any live cell with two or three live neighbors lives on to the next generation.
 3. Any live cell with more than three live neighbors dies, as if by over-population.
 4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 
-## Your task
-
-Create a website, where the grid is shown and game of life is simulated. One should be able to "draw" on the grid, to create cells that are alive. For this you should use the given project structure.
-
 ## Setup
 
-To save you some time and for us to see how you use relevant technologies for us, we created a small boilerplate.
+You extend a non working version of Game Of Life we provide you, to make it work on your machine, follow the steps:
 
-1. Make sure you have an up to date node version with npm. Execute `npm install` in this directory
+1. Make sure you have an up to date node version with npm and execute `npm install` in this directory.
 2. Execute `npm start` to compile the project, it is now served at [http://localhost:3000/](http://localhost:3000/).
 3. Changes can be made in `src/index.js`. You can also add additional files etc.
 
+We already decided which technologies to use for this project, mainly React, styled-components and webpack with babel. The provided codebase should offer enough examples on how to use them in the most basic way.
+
+## Your task
+
+1. Change the `createBoard` method to randomly set `false`/`true` for each cell.
+2. Implement the `setAlive` method, which sets a cell to true. It should be shown as black, when functional.
+3. Make the Game Of Life simulator functional by implementing the `getNeighbours` and `step` function.
+4. Make it possible, that one can actually draw the cells, instead of clicking on each of them.
+5. Implement a _PLAY_ and _PAUSE_ button. When the play button is active, a new iteration should be created every 200ms.
+6. Bonus (optional): Implement a history, which you can access using a _PREVIOUS ITERATION_ Button. The current iteration should be shown using a counter somewhere.
+
 ## Questions to consider
 
-1. Why did you use this data structure?
-2. Why did you use dom/canvas 2d/webgl for rendering? What are the pros/cons for each?
-3. How would you go to create a general purpose cellular automata?
-4. Did it make sense to build with React? What were problems in comparison to vanilla JS?
+1. Why did we use dom for rendering instead of canvas 2d / webgl? What are the pros/cons for each?
+2. How would you go to create a general purpose cellular automata? (How to make the rules adjustable)
+3. Did it make sense to build with React? What were problems in comparison to vanilla JS?
--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled, {injectGlobal} from 'styled-components';
 
+const COLORS = {
+  primary: 'yellow',
+};
+
+//
+// UTILITY FUNCTIONS
+//
+
+// range(5) => [0, 1, 2, 3, 4]
+function range(n) {
+  return Array(n).fill().map((_, i) => i);
+}
+
+
+//
+// GLOBAL STYLES
+//
+
 injectGlobal`
   * {
     box-sizing: border-box;
@@ -14,6 +32,10 @@ injectGlobal`
   }
 `;
 
+//
+// STYLED COMPONENTS
+//
+
 const Container = styled.div`
   max-width: 800px;
   padding: 40px;
@@ -24,17 +46,137 @@ const GameContainer = styled.div`
   position: relative;
   width: 100%;
   height: 500px;
-  border: 1px solid black;
+  border: 2px solid black;
 `;
 
-const GameOfLife = () => {
-  return (
-    <Container>
-      <h1>{'My Own Game of Life'}</h1>
-      <GameContainer>
-      </GameContainer>
-    </Container>
-  );
+const Cell = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  border: 1px solid #EEE;
+  background: ${props => props.alive ? 'black' : 'white'};
+
+  &:hover {
+    background: ${COLORS.primary};
+  }
+`;
+
+const Button = styled.button`
+  text-transform: uppercase;
+  margin-top: 5px;
+  border-radius: 0;
+  background: white;
+  border: 2px solid black;
+  padding: 10px;
+
+  &:hover {
+    background: ${COLORS.primary};
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+//
+// GOL
+//
+
+class GameOfLife extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setBoardContainer = this.setBoardContainer.bind(this);
+    this.state = {
+      cells: [],
+      cellSize: 20,
+      height: 1,
+      width: 1,
+      rows: 1,
+      columns: 1,
+    }
+  }
+
+  setBoardContainer(dom) {
+    this.boardContainer = dom;
+    this.updateBoard();
+    window.addEventListener('resize', () => this.updateBoard());
+  }
+
+  updateBoard() {
+    const height = this.boardContainer.clientHeight;
+    const width = this.boardContainer.clientWidth;
+    // a column is also called x sometimes
+    const columns = Math.floor(width / this.state.cellSize);
+    // a row is also called y sometimes
+    const rows = Math.floor(height / this.state.cellSize);
+    const cells = this.createBoard(columns, rows);
+
+    this.setState({
+      height,
+      width,
+      cells,
+      rows,
+      columns,
+    });
+  }
+
+  createBoard(columns, rows) {
+    return range(columns).map(i => {
+      return range(rows).map(_ => false);
+    });
+  }
+
+  // set a cell alive
+  setAlive(x, y) {
+  }
+
+  // get the alive neighbours of a cell
+  getNumberOfAliveNeighbours(x, y) {
+  }
+
+  // iteration step, use the rules to determine which cells should be alive
+  step() {
+  }
+
+  render() {
+
+    const skewX = this.state.width / (this.state.columns * this.state.cellSize);
+    const skewY = this.state.height / (this.state.rows * this.state.cellSize);
+
+    return (
+      <Container>
+        <h1>{'My Own Game of Life'}</h1>
+        <GameContainer innerRef={this.setBoardContainer}>
+          <div
+            style={{
+              transform: `scale(${skewX}, ${skewY})`,
+              transformOrigin: 'top left',
+            }}
+          >
+            {this.state.cells.map((column, x) => {
+              return column.map((alive, y) => {
+                return (
+                  <Cell
+                    onClick={() => this.setAlive(x, y)}
+                    key={`${y}_${x}`}
+                    alive={alive}
+                    style={{
+                      transform: `translate(${x * this.state.cellSize}px, ${y * this.state.cellSize}px)`,
+                      height: this.state.cellSize,
+                      width: this.state.cellSize,
+                    }}
+                  />
+                );
+              });
+            })}
+          </div>
+        </GameContainer>
+        <Button onClick={() => this.step()}>
+          {'Next Iteration'}
+        </Button>
+      </Container>
+    );
+  }
 }
 
 ReactDOM.render(
